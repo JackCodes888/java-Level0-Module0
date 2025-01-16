f323af20345b233f6a24fb3fef12376eb321eb51
JackCodes888 <jack.stewart.hay@gmail.com>
2025-01-16 00:57:54 +0000

    I finished lesson 3. 1/15/25

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,15 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String answer = JOptionPane.showInputDialog("Do you know how to code.");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if (answer.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null,"You should rule the world." );
+		}		
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+		if (answer.equalsIgnoreCase("no")) {
+			JOptionPane.showMessageDialog(null,"You need to sign up for classes at The Leauge.");
+		}
 	}
 }
 
